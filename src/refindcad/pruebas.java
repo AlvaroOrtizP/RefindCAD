7b2203067b143286b49312b420c84ca9a3568843
alorp <alorpes@gmail.com>
2021-03-10 14:01:51 +0100

    Añadidos los metodos de la tabla Favorito

--- src/refindcad/pruebas.java
+++ src/refindcad/pruebas.java
@@ -6,6 +6,8 @@
 package refindcad;
 
 import Controlador.RefindCAD;
+import POJOS.Anuncio;
+import POJOS.Categoria;
 import POJOS.ExcepcionRefind;
 import POJOS.Usuario;
 import java.sql.Connection;
@@ -13,6 +15,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -31,6 +34,9 @@ public class pruebas {
         do {
             System.out.println("Opcion 1: insertar usuario");
             System.out.println("Opcion 2: mostrar un usuario");
+            System.out.println("Opcion 3: insertar favorito");
+            System.out.println("Opcion 4: eliminar un favorito");
+            System.out.println("Opcion 5: obtener favoritos");
             opcion = scan.nextLine();
 
             switch (opcion) {
@@ -60,8 +66,53 @@ public class pruebas {
                         System.out.println(ex.getMensajeAdmin());
                     }
                     break;
+                case "3":
+                    System.out.println("Insertar un favorito");
+
+                    try {
+                        RefindCAD refindCAD = new RefindCAD();
+                        Usuario usuario = new Usuario("asdasd", "Ruben", "Lara", "email@gmail.com", "asdasda asdasd ", "asdasd", 0);
+                        Categoria categoria = new Categoria(1, "asd", "asd", "asd");
+                        Anuncio anuncio = new Anuncio(3, "asd", "qewqwe", categoria, usuario, "5664565", "564654");//Error al poner una categoria o anuncio no existente
+                        refindCAD.insertarFavorito(usuario, anuncio);
+
+                        System.out.println(usuario);
+                    } catch (ExcepcionRefind ex) {
+                        System.out.println(ex.getMensajeUsuario());
+                        System.out.println(ex.getMensajeAdmin());
+                    }
+                    break;
+                case "4":
+                    System.out.println("Eliminar un favorito");
+
+                    try {
+                        RefindCAD refindCAD = new RefindCAD();
+                        Usuario usuario = new Usuario("asdasd", "Ruben", "Lara", "email@gmail.com", "asdasda asdasd ", "asdasd", 0);
+                        Categoria categoria = new Categoria(1, "asd", "asd", "asd");
+                        Anuncio anuncio = new Anuncio(3, "asd", "qewqwe", categoria, usuario, "5664565", "564654");//Error al poner una categoria o anuncio no existente
+                        refindCAD.eliminarFavorito(usuario, anuncio);
+
+                        System.out.println(usuario);
+                    } catch (ExcepcionRefind ex) {
+                        System.out.println(ex.getMensajeUsuario());
+                        System.out.println(ex.getMensajeAdmin());
+                    }
+                    break;
+                case "5":
+                    System.out.println("Obtener favoritos");
+                    ArrayList<Anuncio> listaAnuncios = new ArrayList<>();
+                    try {
+                        RefindCAD refindCAD = new RefindCAD();
+                        listaAnuncios = refindCAD.obtenerFavoritos();
+                        System.out.println(listaAnuncios.get(0).getTitulo());
+                    } catch (ExcepcionRefind ex) {
+                        System.out.println(ex.getMensajeUsuario());
+                        System.out.println(ex.getMensajeAdmin());
+                    }
+                    break;
 
             }
+            opcion = "kk";
         } while (opcion.equals("kk"));
     }
 
